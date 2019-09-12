3d23617dbef0550d5b32d7a1cb5e6232257868c9
hschoenburg <hschoenburg@gmail.com>
2019-09-11 17:24:24 -0700

    use webpacks proxy and axios for querying REST server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+/*
+ * ok lets spec out this app. It should work for hellochain first
+ * Create an account
+ * So Send Greetings to an address
+ * List all greetings for an address
+ */
+
+
+import axios from "axios"
 import _ from 'lodash';
 import {encodeString, decodeString } from '@tendermint/amino-js'
 import { marshalTx, unmarshalTx } from '@tendermint/amino-js';
@@ -8,73 +17,89 @@ import {
   // createSignature,
   // createSignMessage,
   generateWalletFromSeed,
-  // generateSeed,
+  generateSeed,
   // generateWallet,
   createSignedTx
   // createBroadcastBody
 } from "js-cosmos-wallet";
 
+//placeholder var for local wallet
+let wallet = {}
 
-const tx = {
-   'type':  'auth/StdTx',
-   'value': {
-       'msg':        [{
-           'type':  'hellochain/MsgGreet',
-           'value': {
-               'Sender': 'cosmos1h806c7khnvmjlywdrkdgk2vrayy2mmvf9rxk2r',
-               'Recipient':   'cosmos1z7g5w84ynmjyg0kqpahdjqpj7yq34v3suckp0e',
-               'Body':    'Greetings Earthling'
-           }
-       }],
-       "fee":        {
-           "amount": [{
-               "denom":  "uatom",
-               "amount": "5000"
-           }],
-           "gas":    "200000"
-       },
-       "signatures": [{
-           "pub_key":   {
-               "type":  "tendermint/PubKeySecp256k1",
-               "value": "AtQaCqFnshaZQp6rIkvAPyzThvCvXSDO+9AzbxVErqJP"
-           },
-           "signature": "1nUcIH0CLT0/nQ0mBTDrT6kMG20NY/PsH7P2gc4bpYNGLEYjBmdWevXUJouSE/9A/60QG9cYeqyTe5kFDeIPxQ=="
-       }],
-       "memo":       "1122672754"
-   }
-};
 
-function encodedTx() {
-  const encodedTx = marshalTx(tx);
-  const decodedTx = unmarshalTx(encodedTx);
-  const second = document.createElement('p');
+function BuildGreetingTx(params) {
 
-  second.innerHTML = JSON.stringify(decodedTx.value.msg[0].value.Body)
-
-  return second
+  const tx = {
+     'type':  'auth/StdTx',
+     'value': {
+         'msg':        [{
+             'type':  'hellochain/MsgGreet',
+             'value': {
+                 'Sender': params.Sender,
+                 'Recipient': params.Recipient,
+                 'Body': params.Body
+             }
+         }],
+         "fee":        {
+             "amount": [{
+                 "denom":  "stake",
+                 "amount": "50"
+             }],
+             "gas":    "200"
+         },
+         "memo":       "I hope this works"
+     }
+  };
+  return tx;
 }
 
-function button(callback, text) {
-  console.log("hello from button")
-  const box = document.createElement("div");
-  const label = document.createElement("p");
-  label.innerHTML = text
-  const button = document.createElement("input")
-  button.setAttribute("type", "submit")
-  button.addEventListener("click", callback)
-  box.appendChild(label)
-  box.appendChild(button)
-  return box
+function generateWallet(e) {
+  e.preventDefault();
+  console.log('seed that wallet!')
+
+  let seed = generateSeed();
+  wallet  = generateWalletFromSeed(seed)
+  document.getElementById("Mnemonic").innerHTML = "Seed: " + seed
+  document.getElementById("PubKey").innerHTML = "PubKey: " + wallet.publicKey
+  document.getElementById("PrivKey").innerHTML = "PrivKey: " + wallet.privateKey
+  document.getElementById("Address").innerHTML = "Address: " + wallet.cosmosAddress
+  document.getElementById("Wallet").style.display = "block"
+  console.log(seed)
+  console.log(wallet)
+
+  getMetadata(wallet.cosmosAddress).then(function(data) {
+    console.log(data)
+  })
+
 }
 
 
 document.addEventListener('DOMContentLoaded', function() {
 
-  let copy = "here is the first buttonreally "
-  let callback = function() {
-    console.log("button clicked")
-  }
-  document.body.appendChild(button(callback, copy))
+  document.getElementById("GenerateWallet").addEventListener('click', generateWallet)
+
+  const GreeterForm = document.getElementById("GreeterForm")
+  GreeterForm.addEventListener("submit", function(e) {
+
+    e.preventDefault()
+    let elements = GreeterForm.elements
+
+    let txParams = {
+      Sender: wallet.cosmosAddress,
+      Recipient:  elements[1].value,
+      Body: elements[0].value
+    }
+
+    let unsignedTx = BuildGreetingTx(txParams)
+    console.log(unsignedTx)
+    let signedTx = sign(unsignedTx, wallet)
+
+    console.log(signedTx)
+
+  })
 
 })
 
+function getMetadata (address) {
+  return axios.get("/auth/accounts/" + address)
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,25 @@
 const path = require('path');
 
 module.exports = {
-mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+  mode: 'development',
+    entry: './src/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+  resolve: {
+          // options for resolving module requests
+          // (does not apply to resolving to loaders)
+          modules: ['node_modules'],
+          // directories where to look for modules
+          extensions: [ ".js",".ts", ".vue"]
   },
-resolve: {
-        // options for resolving module requests
-        // (does not apply to resolving to loaders)
-        modules: ['node_modules'],
-        // directories where to look for modules
-        extensions: [ ".js",".ts", ".vue"]
-},
 
 
-devServer: {
-  contentBase: './dist'
-   }
-};
+  devServer: {
+    contentBase: './dist',
+  proxy: {
+          '/auth': 'http://localhost:1317'
+        }
+  }
+}
